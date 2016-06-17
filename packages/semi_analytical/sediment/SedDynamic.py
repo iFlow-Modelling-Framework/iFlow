7246045a780b2a3f2b7a0a91785aa30193230f9e
Ronald Brouwer <r.l.brouwer@tudelft.nl>
2016-06-17 14:04:20 +0200

    changed construction of z array

--- packages/semi_analytical/sediment/SedDynamic.py
+++ packages/semi_analytical/sediment/SedDynamic.py
@@ -7,6 +7,7 @@ import numpy as np
 from scipy import integrate
 from nifty.harmonicDecomposition import absoluteU, signU
 from src.DataContainer import DataContainer
+import nifty as ny
 import logging
 
 
@@ -40,8 +41,7 @@ class SedDynamic:
         self.dx = self.x[1:]-self.x[:-1]
         kmax = self.input.v('grid', 'maxIndex', 'z')
         self.z = self.input.v('grid', 'axis', 'z', 0, range(0, kmax+1))
-        self.zarr = self.z.reshape(1, len(self.z)) * (self.input.v('-H', x=self.x/self.L).reshape(len(self.x), 1) +
-                                                      self.input.v('R', x=self.x / self.L).reshape(len(self.x), 1))
+        self.zarr = ny.dimensionalAxis(self.input.slice('grid'), 'z')[:, :, 0]
         self.Av0 = self.input.v('Av', x=self.x/self.L, z=0, f=0).reshape(len(self.x), 1).reshape(len(self.x), 1)
         self.Av0x = self.input.d('Av', x=self.x/self.L, z=0, f=0, dim='x').reshape(len(self.x), 1)
         self.H = (self.input.v('H', x=self.x/self.L).reshape(len(self.x), 1) +
