6b430c023d2dba543b2ff21a998706b36d4d486e
Yoeri Dijkstra <y.m.dijkstra@tudelft.nl>
2017-05-02 10:29:09 +0200

    removed several file, not ready yet

--- packages/numerical1DV/kepsilon/setupCompiler.bat
+++
@@ -1,4 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC"\vcvarsall.bat x86
-call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\"vsvars32.bat
-call "C:\Program Files (x86)\Intel\Composer XE 2011 SP1\bin\"ipsxe-comp-vars.bat ia32 vs2010
-set VS90COMNTOOLS=%VS100COMNTOOLS%
