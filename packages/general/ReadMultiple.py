243f6ae0849c5f6ad3f3233c6636497ae9838f2e
Yoeri Dijkstra <y.m.dijkstra@tudelft.nl>
2017-09-06 10:16:49 -0400

    ReadMultiple allows to read from multiple folders

--- packages/general/ReadMultiple.py
+++ packages/general/ReadMultiple.py
@@ -2,6 +2,7 @@
 ReadMultiple
 
 Date: 11-Nov-15
+Update: 13-07-17 - allow for reading files from multiple folders
 Authors: Y.M. Dijkstra
 """
 import nifty as ny
@@ -21,23 +22,47 @@ class ReadMultiple:
 
     def run(self):
         self.logger.info('Reading files')
+        ## set folder(s)
         cwdpath = self.input.v('CWD') or ''  # path to working directory
-        folder = os.path.join(cwdpath, self.input.v('folder'))
+        folders = ny.toList(self.input.v('folder'))
+        folders = [os.path.join(cwdpath, i) for i in folders]
 
-        files = ny.toList(self.input.v('files'))
-        # If files is 'all' then get all files that have a .p extension
-        if files[0] == 'all':
-            files = [ f[:-2] for f in os.listdir(folder) if os.path.isfile(os.path.join(folder,f)) and f.endswith('.p') ]
+        ## determine unique part of folder names
+        folder_name = []
+        for folder in folders:
+            tempname = folder.replace('\\', '/')
+            folder_name.append(tempname.split('/'))
+        nameparts = list(set([item for sublist in folder_name for item in sublist]))
+        nameinall = [all([i in j for j in folder_name]) for i in nameparts]
+        nameparts = [prt for i, prt in enumerate(nameparts) if nameinall[i]]
+        for i, folder in enumerate(folder_name):
+            for name in nameparts:
+                try:
+                    folder_name[i].remove(name)
+                except:
+                    pass
+            if len(folder_name[i])>0:
+                folder_name[i].append('')
 
-        # load data from the files
+        ## Loop over folders
         experimentdata = {}
-        for file in files:
-            if self.input.v('variables') == 'all':
-                varlist = None
+        for i, folder in enumerate(folders):
+            ## scan files
+            files = ny.toList(self.input.v('files'))
+            # If files is 'all' then get all files that have a .p extension, else take the listing files
+            if files[0] == 'all':
+                files = [f[:-2] for f in os.listdir(folder) if os.path.isfile(os.path.join(folder,f)) and f.endswith('.p') ]
             else:
-                varlist = ny.toList(self.input.v('variables'))+['grid']
-            d = ny.pickleload(os.path.join(folder, file), varlist)
-            experimentdata[file] = DataContainer(d)
+                files = [f for f in ny.toList(self.input.v('files')) if os.path.isfile(os.path.join(folder,f)) and f.endswith('.p')]
+
+            ## load data from the files
+            for file in files:
+                if self.input.v('variables') == 'all':
+                    varlist = None
+                else:
+                    varlist = ny.toList(self.input.v('variables'))+['grid']
+                d = ny.pickleload(os.path.join(folder, file), varlist)
+                experimentdata['/'.join(folder_name[i]) + file] = DataContainer(d)
 
         d = {}
         d['experimentdata'] = experimentdata
