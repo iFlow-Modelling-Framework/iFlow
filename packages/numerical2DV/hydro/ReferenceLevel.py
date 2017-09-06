Critical corrections to the numerical2DV model; previous solutions may have been wrong

packages/numerical2DV/hydro/ReferenceLevel.py
@@ -67,7 +67,7 @@ class ReferenceLevel:
         jmax = self.input.v('grid', 'maxIndex', 'x')
 
         # Construct reference level by stepping from x=0 towards x=L
-        R = np.zeros(jmax+1)
+        R = self.input.v('R', range(0, jmax+1))
         x = ny.dimensionalAxis(self.input.slice('grid'), 'x')[:,0,0]
         dx = x[1:]-x[0:-1]
         for j in range(0, jmax):

packages/numerical2DV/sediment/SedDynamicLead.py
@@ -8,6 +8,7 @@ import logging
 import numpy as np
 import nifty as ny
 from cFunction import cFunction
+from cFunctionUncoupled import cFunctionUncoupled
 from erosion import erosion
 
 
@@ -31,6 +32,9 @@ class SedDynamicLead:
         self.submodulesToRun = self.input.v('submodules')
         # H = self.input.v('H', range(0, jmax+1))
         method = self.input.v('erosion_formulation')
+        frictionpar = self.input.v('friction')      # friction parameter used for the erosion, by default the total roughness
+        if frictionpar == None:
+            frictionpar = 'Roughness'
 
         ################################################################################################################
         # Left hand side
@@ -59,24 +63,82 @@ class SedDynamicLead:
         Fbed = np.zeros([jmax+1, 1, ftot, 1], dtype=complex)
 
         # erosion
-        E = erosion(ws, Av, 0, self.input, method)
+        E = erosion(ws, 0, self.input, method, friction=frictionpar)
         Fbed[:, :, fmax:, 0] = -E
+
+
 
         ################################################################################################################
         # Solve equation
         ################################################################################################################
+        # Coupled system
         c, cMatrix = cFunction(ws, Kv, F, Fsurf, Fbed, self.input, hasMatrix = False)
         c = c.reshape((jmax+1, kmax+1, ftot))
-
         hatc0 = ny.eliminateNegativeFourier(c, 2)
+
+        # Uncoupled system
+        # hatc0 = np.zeros((jmax+1, kmax+1, fmax+1), dtype=complex)
+        # hatc0[:, :, 0] = cFunctionUncoupled(ws, Kv, F, Fsurf, Fbed, self.input, 0).reshape((jmax+1, kmax+1))
+        # hatc0[:, :, 2] = cFunctionUncoupled(ws, Kv, F, Fsurf, Fbed, self.input, 2).reshape((jmax+1, kmax+1))
+
         # correction at the bed (optional)
         # cbed00 = E[:, -1, 0]/ws[:, -1, 0]
         # frac = cbed00/hatc0[:, -1, 0]
         # hatc0 = hatc0*frac.reshape((jmax+1, 1, 1))
 
+        ## analytical solutions
+        # ahatc0 = np.zeros((jmax+1, kmax+1, fmax+1), dtype=complex)
+        # ahatc02 = np.zeros((jmax+1, kmax+1, fmax+1), dtype=complex)
+        # z = ny.dimensionalAxis(self.input.slice('grid'), 'z')[:, :, 0]
+        # ws = ws[:, 0, 0]
+        # Kv = Kv[:, 0, 0]
+        # OMEGA = self.input.v('OMEGA')
+        # H = self.input.v('H', range(0, jmax+1))
+        #
+        # # M0
+        # ahatc0[:, :, 0] = (E[:, 0, 0]/ws).reshape((jmax+1, 1))*np.exp(-(ws/Kv).reshape((jmax+1, 1))*(z+H.reshape((jmax+1, 1))))
+        #
+        #
+        # # M4
+        #
+        # r1 = -ws/(2.*Kv)+np.sqrt(ws**2+8*1j*OMEGA*Kv)/(2*Kv)
+        # r2 = -ws/(2.*Kv)-np.sqrt(ws**2+8*1j*OMEGA*Kv)/(2*Kv)
+        # k2 = -E[:, 0, 2]*(Kv*(-r1*(ws+Kv*r2)/(ws+Kv*r1)*np.exp(-r1*H) + r2*np.exp(-r2*H)))**-1.
+        # k1 = -k2*(ws+Kv*r2)/(ws+Kv*r1)
+        # ahatc0[:, :, 2] = k1.reshape((jmax+1, 1))*np.exp(r1.reshape((jmax+1, 1))*z) + k2.reshape((jmax+1, 1))*np.exp(r2.reshape((jmax+1, 1))*z)
+
+        # import step as st
+        # import matplotlib.pyplot as plt
+        #
+        # st.configure()
+        # plt.figure(1, figsize=(1,3))
+        # plt.subplot(1,3,1)
+        # plt.plot(np.real(ahatc0[0, :, 0]), z[0, :], label='analytical')
+        # plt.plot(np.real(hatc0[0, :, 0]), z[0, :], label='numerical')
+        # plt.xlim(0, np.max(np.maximum(abs(ahatc0[0, :, 0]), abs(hatc0[0, :, 0])))*1.05)
+        #
+        # plt.subplot(1,3,2)
+        # plt.plot(np.abs(ahatc0[0, :, 2]), z[0, :], label='analytical')
+        # # plt.plot(np.abs(ahatc02[0, :, 2]), z[0, :], label='analytical2')
+        # plt.plot(np.abs(hatc0[0, :, 2]), z[0, :], label='numerical')
+        # # plt.xlim(np.min(np.minimum(abs(ahatc0[0, :, 2]), abs(hatc0[0, :, 2])))*1.05, np.max(np.maximum(abs(ahatc0[0, :, 2]), abs(hatc0[0, :, 2])))*1.05)
+        # plt.legend()
+        #
+        # plt.subplot(1,3,3)
+        # plt.plot(np.imag(ahatc0[0, :, 2]), z[0, :], label='analytical')
+        # # plt.plot(np.imag(ahatc02[0, :, 2]), z[0, :], label='analytical2')
+        # plt.plot(np.imag(hatc0[0, :, 2]), z[0, :], label='numerical')
+        # # plt.xlim(np.min(np.minimum(abs(ahatc0[0, :, 2]), abs(hatc0[0, :, 2])))*1.05, np.max(np.maximum(abs(ahatc0[0, :, 2]), abs(hatc0[0, :, 2])))*1.05)
+        # plt.legend()
+        # st.show()
+
+
         d['hatc0'] = {}
         d['hatc0']['a'] = {}
         d['hatc0']['a']['erosion'] = hatc0
+
+
+
         return d
 
 

packages/numerical2DV/sediment/cFunction.py
@@ -34,15 +34,13 @@ def cFunction(ws, Kv, F, Fsurf, Fbed, data, hasMatrix = False):
     # build, save and solve the matrices in every water column
     ####################################################################################################################
     for j in range(0, jmax+1):
-        try:
-            z = ny.dimensionalAxis(data.slice('grid'), 'z')[j,:,0]
-        except:
-            z = -np.linspace(0, 1, kmax+1)
+        z = ny.dimensionalAxis(data.slice('grid'), 'z')[j, :, 0]
         dz = z[1:]-z[:-1]
         del z
         dz = dz.reshape((len(dz), 1, 1))
         dz_down = dz[:-1, Ellipsis]
         dz_up = dz[1:, Ellipsis]
+        dz_av = 0.5*(dz_up+dz_down)
 
         ##### LEFT HAND SIDE #####
         N = np.zeros([kmax+1, 2*bandwidth+1, ftot], dtype=complex)
@@ -68,7 +66,7 @@ def cFunction(ws, Kv, F, Fsurf, Fbed, data, hasMatrix = False):
             a = - 0.5*(N[0:-2, :, :]+N[1:-1, :, :])/dz_down                                         + Ws[0:-2, :, :]      # for k-1: from 1..kmax-1
             b =   0.5*(N[0:-2, :, :]+N[1:-1, :, :])/dz_down + 0.5*(N[1:-1, :, :]+N[2:, :, :])/dz_up - Ws[1:-1, :, :]        # for k: from 1..kmax-1
             c =                                             - 0.5*(N[1:-1, :, :]+N[2:, :, :])/dz_up                       # for k+1: from 1..kmax
-            b[:, bandwidth, :] += 0.5*(np.arange(-fmax, ftot-fmax)*1j*OMEGA).reshape((1, ftot))*dz_up.reshape((kmax-1, 1))
+            b[:, bandwidth, :] += (np.arange(-fmax, ftot-fmax)*1j*OMEGA).reshape((1, ftot))*dz_av.reshape((kmax-1, 1))
 
             a = np.swapaxes(a, 0, 1)
             b = np.swapaxes(b, 0, 1)
@@ -81,7 +79,7 @@ def cFunction(ws, Kv, F, Fsurf, Fbed, data, hasMatrix = False):
 
             ## surface
             b =   Ws[[0],Ellipsis]-N[[0],Ellipsis]/dz[[0], Ellipsis]
-            c =   N[[0],Ellipsis]/dz[[0]]
+            c =   +N[[0],Ellipsis]/dz[[0]]
 
             b = np.swapaxes(b, 0, 1)
             c = np.swapaxes(c, 0, 1)
@@ -90,8 +88,8 @@ def cFunction(ws, Kv, F, Fsurf, Fbed, data, hasMatrix = False):
             A[:2*bandwidth+1, ftot:2*ftot] = (c.reshape(c.shape[0], c.shape[1]*c.shape[2]))
 
             ## bed
-            a = N[[-1],Ellipsis]/dz[[-1]]
-            b = -N[[-1],Ellipsis]/dz[[-1]]
+            a = -N[[-1],Ellipsis]/dz[[-1]]
+            b = N[[-1],Ellipsis]/dz[[-1]]
 
             a = np.swapaxes(a, 0, 1)
             b = np.swapaxes(b, 0, 1)
@@ -114,7 +112,7 @@ def cFunction(ws, Kv, F, Fsurf, Fbed, data, hasMatrix = False):
 
         RHS[ftot:-ftot, :] = (F[j, 1:-1, Ellipsis]*dz_up).reshape(((F.shape[1]-2)*F.shape[2], nRHS))
         RHS[:ftot, :] = Fsurf[j, 0, Ellipsis]
-        RHS[-ftot:, :] += -Fbed[j, 0, Ellipsis]
+        RHS[-ftot:, :] += Fbed[j, 0, Ellipsis]
 
         ################################################################################################################
         # Solve

packages/numerical2DV/sediment/shearstress.py
@@ -1,7 +1,104 @@
 import numpy as np
 import nifty as ny
+import step as st
+import matplotlib.pyplot as plt
 
-def shearstress(Av, tau_order, data, submodule=None):
+def shearstress(tau_order, data, submodule=None, friction='Roughness'):
+    return shearstressCheb(tau_order, data, submodule=submodule, friction=friction)
+
+def comparestresses(tau_order, data, submodule=None, friction='Roughness'):
+    taub_abs = shearstressGS(tau_order, data, submodule, friction=friction)
+    taub = shearstressCheb(tau_order, data, submodule, friction=friction)
+    taub_exact = shearstress_exact(tau_order, data, submodule, friction=friction)
+
+    st.configure()
+    plt.figure(1,figsize=(1,3))
+    for n in range(0, 3):
+        plt.subplot(1, 3, n+1)
+        plt.plot(np.real((taub[:-1, 0, n]-taub_exact[:-1, 0, n])/(taub_exact[:-1, 0, n]+10**-10)), label='Cheb')
+        plt.plot(np.real((taub_abs[:-1, 0, n]-taub_exact[:-1, 0, n])/(taub_exact[:-1, 0, n]+10**-10)), label='GS')
+        plt.legend()
+    plt.figure(2,figsize=(1,3))
+    for n in range(0, 3):
+        plt.subplot(1, 3, n+1)
+        plt.plot(np.imag((taub[:-1, 0, n]-taub_exact[:-1, 0, n])/(taub_exact[:-1, 0, n]+10**-10)), label='Cheb')
+        plt.plot(np.imag((taub_abs[:-1, 0, n]-taub_exact[:-1, 0, n])/(taub_exact[:-1, 0, n]+10**-10)), label='GS')
+        plt.legend()
+
+    st.show()
+
+def shearstressGS(tau_order, data, submodule=None, friction='Roughness'):     # Shear stress following the formulation of Schramkowski
+    jmax = data.v('grid', 'maxIndex', 'x')
+    kmax = data.v('grid', 'maxIndex', 'z')
+    fmax = data.v('grid', 'maxIndex', 'f')
+    rho0 = data.v('RHO0')
+    sf = data.v(friction, range(0, jmax+1), 0, 0)
+
+    if submodule is None:
+        submodule = (None, )*(tau_order+1)
+
+    ## 1. bed shear stress
+    # the bed shear stress is extended over fmax+1 frequency components to prevent inaccuracies in truncation
+    ulist = []
+    for i in range(0, tau_order+1):
+        if submodule[i] is None:
+            u = data.v('u'+str(i), range(0, jmax+1), [kmax], range(0, fmax+1))
+        else:
+            u = data.v('u'+str(i), submodule[i], range(0, jmax+1), [kmax], range(0, fmax+1))
+        if u is None:
+            u = np.zeros((jmax+1, 1, fmax+1), dtype=complex)
+
+        ulist.append(u)
+
+    taub_abs = np.zeros((jmax+1, 1, fmax+1), dtype=complex)
+    if tau_order == 0:
+        uabs0 = ny.absoluteU(ulist[0][:, 0, 1]+10**-6, 0)
+        uabs2 = ny.absoluteU(ulist[0][:, 0, 1]+10**-6, 2)+np.conj(ny.absoluteU(ulist[0][:, 0, 1]+10**-6, -2))
+        taub_abs[:, 0, 0] = rho0*sf*uabs0
+        taub_abs[:, 0, 2] = rho0*sf*uabs2
+    elif tau_order ==1:
+        signu = np.zeros((jmax+1, 1, np.maximum(fmax+1, 4)), dtype=complex)
+        signu[:, 0, 1] = ny.signU(ulist[0][:, 0, 1]+10**-6, 1) + np.conj(ny.signU(ulist[0][:, 0, 1]+10**-6, -1))
+        signu[:, 0, 3] = ny.signU(ulist[0][:, 0, 1]+10**-6, 3) + np.conj(ny.signU(ulist[0][:, 0, 1]+10**-6, -3))
+        if fmax+1 < 4:
+            ulist[1] = np.concatenate((ulist[1], np.zeros((jmax+1, 1, 4-fmax-1))), 2)
+        taub_abs = rho0*sf.reshape((jmax+1, 1, 1))*ny.complexAmplitudeProduct(ulist[1], signu, 2)
+    elif tau_order ==2:
+        uabs = np.abs(ulist[1][:, 0, 0])        # only for subtidal flows
+        taub_abs[:, 0, 0] = rho0*sf*uabs
+
+    return taub_abs[:, :, :fmax+1]
+
+def shearstress_exact(tau_order, data, submodule=None, friction='Roughness'):     # Shear stress derived using time series (ordering is difficult)
+    jmax = data.v('grid', 'maxIndex', 'x')
+    kmax = data.v('grid', 'maxIndex', 'z')
+    fmax = data.v('grid', 'maxIndex', 'f')
+    rho0 = data.v('RHO0')
+    sf = data.v(friction, range(0, jmax+1), 0, 0)
+
+    if submodule is None:
+        submodule = (None, )*(tau_order+1)
+
+    ## 1. bed shear stress
+    # the bed shear stress is extended over fmax+1 frequency components to prevent inaccuracies in truncation
+    ulist = []
+    for i in range(0, tau_order+1):
+        if submodule[i] is None:
+            u = data.v('u'+str(i), range(0, jmax+1), [kmax], range(0, fmax+1))
+        else:
+            u = data.v('u'+str(i), submodule[i], range(0, jmax+1), [kmax], range(0, fmax+1))
+        if u is None:
+            u = np.zeros((jmax+1, 1, fmax+1), dtype=complex)
+
+        ulist.append(u)
+    u = sum(ulist)
+    utim = ny.invfft(np.concatenate((u, np.zeros((jmax+1, 1, 500))), 2), 2)
+    utim = np.abs(utim)
+    uabs = ny.fft(utim, 2)[:, :, :fmax+1]
+    taub_abs = rho0*sf.reshape((jmax+1, 1, 1))*uabs
+    return taub_abs
+
+def shearstressCheb(tau_order, data, submodule=None, friction='Roughness'):       # Shear stress using Chebyshev polynomials
     jmax = data.v('grid', 'maxIndex', 'x')
     kmax = data.v('grid', 'maxIndex', 'z')
     fmax = data.v('grid', 'maxIndex', 'f')
@@ -10,18 +107,65 @@ def shearstress(Av, tau_order, data, submodule=None):
 
     ## 1. bed shear stress
     # the bed shear stress is extended over fmax+1 frequency components to prevent inaccuracies in truncation
-    taub = []
-    Av = np.concatenate((Av, np.zeros((jmax+1, kmax+1, fmax+1))), 2)
-    for i in range(0, tau_order+1):
-        if submodule[i] is None:
-            uz = data.d('u'+str(i), range(0, jmax+1), [kmax], range(0, fmax+1), dim='z')
-        else:
-            uz = data.d('u'+str(i), submodule[i], range(0, jmax+1), [kmax], range(0, fmax+1), dim='z')
-        if uz is None:
-            uz = np.zeros((jmax+1, 1, fmax+1), dtype=complex)
+    # Method 1: using Av
 
-        uz = np.concatenate((uz, np.zeros((jmax+1, 1, fmax+1))),2)
-        taub.append(ny.complexAmplitudeProduct(Av[:, [kmax], :], uz, 2))
+    taub1 = []
+    for i in range(0, tau_order+1):
+        taub1.append(0)
+        for j in range(0, i+1):
+            # friction
+            if j == 0:
+                Av = data.v('Av', range(0, jmax+1), range(0, kmax+1), range(0, fmax+1))
+            elif submodule[i] is None or submodule[i]== 'mixing':
+                Av = data.v('Av'+str(i), range(0, jmax+1), range(0, kmax+1), range(0, fmax+1))
+            else:
+                Av = np.zeros((jmax+1, kmax+1, fmax+1))
+            # velocity shear
+            q = i-j
+            if submodule[q] is None:
+                uz = data.d('u'+str(q), range(0, jmax+1), [kmax], range(0, fmax+1), dim='z')
+            else:
+                uz = data.d('u'+str(q), submodule[q], range(0, jmax+1), [kmax], range(0, fmax+1), dim='z')
+            if uz is None:
+                uz = np.zeros((jmax+1, 1, fmax+1), dtype=complex)
+
+            #  extend and multiply
+            Av = np.concatenate((Av, np.zeros((jmax+1, kmax+1, fmax+1))), 2)
+            uz = np.concatenate((uz, np.zeros((jmax+1, 1, fmax+1))),2)
+            taub1[i] += ny.complexAmplitudeProduct(Av[:, [kmax], :], uz, 2)
+
+    # Method 2: using sf (equivalent to using Av)
+    taub = []
+    for i in range(0, tau_order+1):
+        taub.append(0)
+        for j in range(0, i+1):
+            # friction
+            if j == 0:
+                sf = data.v(friction, range(0, jmax+1), [0], range(0, fmax+1))
+            elif submodule[i] is None or submodule[i]== 'mixing':
+                sf = data.v(friction+str(i), range(0, jmax+1), [0], range(0, fmax+1))
+            else:
+                sf = np.zeros((jmax+1, 1, fmax+1))
+            # velocity
+            q = i-j
+            if submodule[q] is None:
+                u = data.v('u'+str(q), range(0, jmax+1), [kmax], range(0, fmax+1))
+            else:
+                u = data.v('u'+str(q), submodule[q], range(0, jmax+1), [kmax], range(0, fmax+1))
+            if u is None:
+                u = np.zeros((jmax+1, 1, fmax+1), dtype=complex)
+            # extend vectors and multiply
+            sf = np.concatenate((sf, np.zeros((jmax+1, 1, fmax+1))), 2)
+            u = np.concatenate((u, np.zeros((jmax+1, 1, fmax+1))),2)
+            taub[i] += ny.complexAmplitudeProduct(sf, u, 2)
+
+    taub = taub1
+    for i, t in enumerate(taub):
+        dif = np.max(np.abs(t - taub1[i]))
+        if dif>1.e-15:
+            print 'test stress ' + str(np.max(np.abs(t - taub1[i])))
+            print 'order ' + str(tau_order)
+
 
     # amplitude
     tau_amp = (np.sum(np.abs(sum(taub)), axis=-1)+10**-3).reshape((jmax+1, 1, 1))