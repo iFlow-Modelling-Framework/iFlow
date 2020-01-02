72d3670bcfa817cfcc0202ff41e14a1550278906
Yoeri Dijkstra <y.m.dijkstra@tudelft.nl>
2020-01-02 13:18:28 +0100

    solver for reverse tidal propagation

--- packages/semi_analytical2DV/hydro/zetaFunctionUncoupled.py
+++ packages/semi_analytical2DV/hydro/zetaFunctionUncoupled.py
@@ -16,7 +16,7 @@ from scipy.linalg import solve_banded
 from nifty import integrate
 
 
-def zetaFunctionUncoupled(n, M, F, Fopen, Fclosed, data, hasMatrix = False):
+def zetaFunctionUncoupled(n, M, F, Fopen, Fclosed, data, hasMatrix = False, reverseBC = False):
     # Init
     jmax = data.v('grid', 'maxIndex', 'x')
     OMEGA = data.v('OMEGA')
@@ -46,16 +46,31 @@ def zetaFunctionUncoupled(n, M, F, Fopen, Fclosed, data, hasMatrix = False):
         A[0, 2:] += c
 
         # Boundary conditions
-        #   Sea (j=0)
-        b = -M[0]/(dx[0]*B[0])
-        b += 0.5*n*1j*OMEGA*dx[0]
-        c = M[1]/(dx[0]*B[0])
+        #   Option 1: force zeta at x=0 and zetax at x=L
+        if not reverseBC:
+            #   Sea (j=0)
+            b = -M[0]/(dx[0]*B[0])
+            b += 0.5*n*1j*OMEGA*dx[0]
+            c = M[1]/(dx[0]*B[0])
 
-        A[1, 0] += b
-        A[0, 1] += c
+            A[1, 0] += b
+            A[0, 1] += c
 
-        #   Weir (j=jmax)
-        A[1, -1] += M[-1]
+            #   Weir (j=jmax)
+            A[1, -1] += M[-1]
+        #   Option 2: force zeta at x=L and zetax at x=0
+        else:
+            #   Sea (j=0)
+            A[1, 0] += M[0]
+
+            #   Weir (j=jmax)
+            b = M[-1]/(dx[-1]*B[-1])
+            b += 0.5*n*1j*OMEGA*dx[-1]
+            a = -M[-2]/(dx[-1]*B[-1])
+
+            A[2, -2] += a
+            A[1, -1] += b
+
     else:
         A = M
 
@@ -63,26 +78,44 @@ def zetaFunctionUncoupled(n, M, F, Fopen, Fclosed, data, hasMatrix = False):
     nRHS = F.shape[-1]
     zRHS = np.zeros([jmax+1, nRHS], dtype=complex)
 
-    # forcing on open boundary
-    zRHS[0, :] = -n*1j*OMEGA*Fopen[0, :]
+    if not reverseBC:
+        # forcing on open boundary
+        zRHS[0, :] = -n*1j*OMEGA*Fopen[0, :]
 
-    # forcing on closed boundary
-    zRHS[-1, :] = Fclosed[0, :]
+        # forcing on closed boundary
+        zRHS[-1, :] = Fclosed[0, :]
 
-    # internal forcing
-    FdivBav = 0.5*(F[1:, :]+F[:-1, :])/B.reshape((jmax, 1))
-    zRHS[1:-1, :] = -FdivBav[1:]+FdivBav[:-1]
-    zRHS[0, :] += -FdivBav[0]
+        # internal forcing
+        FdivBav = 0.5*(F[1:, :]+F[:-1, :])/B.reshape((jmax, 1))
+        zRHS[1:-1, :] = -FdivBav[1:]+FdivBav[:-1]
+        zRHS[0, :] += -FdivBav[0]
+    else:
+        # forcing on open boundary
+        zRHS[0, :] = Fopen[0, :]
+
+        # forcing on closed boundary
+        zRHS[-1, :] = -n*1j*OMEGA*Fclosed[0, :]
+
+        # internal forcing
+        FdivBav = 0.5*(F[1:, :]+F[:-1, :])/B.reshape((jmax, 1))
+        zRHS[1:-1, :] = -FdivBav[1:]+FdivBav[:-1]
+        zRHS[-1, :] += -FdivBav[-1]
 
     ##### SOLVE #####
     zetax = solve_banded((1, 1), A, zRHS, overwrite_ab=False, overwrite_b=True)
     zetax = zetax.reshape(jmax+1, nRHS)
 
     # integrate to zeta
-    zeta = integrate(zetax.reshape((jmax+1, 1, 1, nRHS)), 'x', 0, np.arange(0, jmax+1), data.slice('grid'), INTMETHOD='TRAPEZOIDAL')
-    zeta = zeta.reshape((jmax+1, nRHS))
+    if not reverseBC:
+        zeta = integrate(zetax.reshape((jmax+1, 1, 1, nRHS)), 'x', 0, np.arange(0, jmax+1), data.slice('grid'), INTMETHOD='TRAPEZOIDAL')
+        zeta = zeta.reshape((jmax+1, nRHS))
 
-    zeta += Fopen[0, :]
+        zeta += Fopen[0, :]
+    else:
+        zeta = integrate(zetax.reshape((jmax+1, 1, 1, nRHS)), 'x', jmax  , np.arange(0, jmax+1), data.slice('grid'), INTMETHOD='TRAPEZOIDAL')
+        zeta = zeta.reshape((jmax+1, nRHS))
+
+        zeta += Fclosed[0, :]
 
     return zeta, zetax, A
 
