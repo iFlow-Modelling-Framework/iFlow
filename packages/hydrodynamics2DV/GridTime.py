5b09907dee0d0b7bc72fba17f054b0505e3d8506
Yoeri Dijkstra <yoeridijkstra@tudelft.nl>
2024-01-19 11:23:07 +0100

    files for multiple timescale method

---
+++ packages/hydrodynamics2DV/GridTime.py
@@ -0,0 +1,71 @@
+"""
+Numerical method for Flow MTS_2DV
+Prepare a regular temporal grid for a MTS_2DV
+
+Date: 26-02-2019
+Author: D.D.Bouwman
+
+Based on "RegularGrid.py" by Y.M.Dijkstra
+"""
+from nifty import makeRegularGrid
+
+
+class MTS_Grid:
+    # Variables
+
+    # Methods
+    def __init__(self, input):
+        self.input = input
+        return
+
+    def run(self):
+        """Prepare a regular grid for the time dependent Multiple-Timescale computations
+
+        Returns:
+            Dictionary containing:
+                grid:   dimensions (list) - list [x,z,f,t]
+                        gridType (str) - 'Regular'
+                        axis: x,z,f,t (ndarray - grid axes between 0 and 1
+                        high: x,z,f,t (any) - dimension-full limit belonging to point 1 on the dimensionless axis
+                        low: x,z,f,t (any) - dimension-full limit belonging to point 0 on the dimensionless axis
+                        maxIndex: x,z,f,t (int) - maximum index number
+                outgrid:    idem
+        """
+        d = {}
+
+        dimensions = ['x', 'z', 'f', 't']
+        enclosures = [(0, self.input.v('L')),
+                      (self.input.v('R'), self.input.n('H')),
+                      None,
+                      (0, self.input.v('sn_cycles'))]
+        contraction = [[], ['x'], [], []]     # Enclosures of each dimension depend on these parameters
+        copy = [1, 1, 0, 1]                  # Copy lower-dimensional arrays over these dimensions. 1: yes, copy. 0: no, only keep in the zero-index
+
+        # grid
+        axisTypes = []
+        axisSize = []
+        axisOther = []
+        variableNames = ['xgrid', 'zgrid', 'fgrid', 'tgrid']
+        for var in variableNames:
+            axisTypes.append(self.input.v(var)[0])
+            axisSize.append(self.input.v(var)[1])
+            axisOther.append(self.input.v(var)[2:])
+        d['MTS_grid'] = makeRegularGrid(dimensions, axisTypes, axisSize, axisOther, enclosures, contraction, copy)
+        d['num_sn_cycles'] = self.input.v('sn_cycles')
+
+        # output grid
+        axisTypes = []
+        axisSize = []
+        axisOther = []
+        variableNames = ['xoutputgrid', 'zoutputgrid', 'foutputgrid', 'toutputgrid']
+        for var in variableNames:
+            axisTypes.append(self.input.v(var)[0])
+            axisSize.append(self.input.v(var)[1])
+            axisOther.append(self.input.v(var)[2:])
+        d['MTS_outputgrid'] = makeRegularGrid(dimensions, axisTypes, axisSize, axisOther, enclosures, contraction, copy)
+        d['__outputGrid'] = {}
+        d['__outputGrid']['MTS_grid'] = 'MTS_outputgrid'
+
+        return d
+
+
---
+++ packages/sediment2DV/perturbation/Iterator_Dynamic.py
@@ -0,0 +1,208 @@
+"""
+Dynamic Spring-Neap Iterator
+
+Update: 11-10-2023
+Update Author: Y.M. Dijkstra (reformulation to implicit code and reformulation to controller structure)
+Original date: 24-Jan-19
+Original author: D.D. Bouwman
+"""
+
+import numpy as np
+from src.util import mergeDicts
+import nifty as ny
+import logging
+
+
+class Iterator_Dynamic():
+    # Variables
+    logger = logging.getLogger(__name__)
+    OutputList = ['zeta0', 'zeta1', 'u0', 'u1', 'c0', 'c1', 'a', 'f', 'F', 'T', 'sediment_transport'] # a -> stock. f -> erodibility
+    verticalsize = [1, 1, None, None, None, None, 1, 1, 1, 1, 1]
+    frequencysize = [None, None, None, None, None, None, 1, 1, 1, 1, 1]
+
+    # Methods
+    def __init__(self, input, block):       
+        self.input = input
+        self.block = block
+        return
+        
+    def run(self):
+        ################################################################################################################
+        # Initialise
+        ################################################################################################################
+        jmax = self.input.v('grid', 'maxIndex', 'x')
+        kmax = self.input.v('grid', 'maxIndex', 'z')
+        fmax = self.input.v('grid', 'maxIndex', 'f')
+        store = {}
+
+        # SPRING NEAP INPUT
+        sigmaM0 = self.input.v('OMEGA')
+        sigmaS0 = self.input.v('sigmaS2')
+
+        num_cycles = self.input.v('num_sn_cycles')
+        period_sn0 = np.abs(2 * np.pi / (sigmaS0 - sigmaM0))
+        t2_nsteps = self.input.v('MTS_grid', 'maxIndex', 't')
+        t_axis = self.input.v('MTS_grid', 'axis', 't').flatten()
+
+        t2_range = t_axis*period_sn0*num_cycles
+
+        bc_0, bc_1 = self.calc_subtidal_bcs()
+
+        # DISCHARGE INPUT
+        Qarray = ny.toList(self.input.v('Q1')) # In this case the river discharge (time series) is constant and given by Q1. For changing Q during the spring-neap cycle, this variable has to be varied per iteration
+
+        if len(Qarray)==1:
+            Qarray = Qarray*(t2_nsteps+1)
+
+        elif len(Qarray) == self.input.v('MTS_grid', 'maxIndex', 't')+1:
+            Qarray = Qarray*num_cycles
+
+        elif len(Qarray)!=t2_nsteps+1:
+            from src.util.diagnostics.KnownError import KnownError
+            raise KnownError('Number of elements of Q1 incompatible; should be 1, equal to number of steps per SN cycle or equal to total number of time steps')
+
+        ################################################################################################################
+        # time integrate
+        ################################################################################################################
+        for iteration in range(0, t2_nsteps):
+            self.logger.info('Time integration iteration ' + str(iteration+1) + '/' + str(t2_nsteps))
+            ## 1. Prepare input
+            #       Offer the input only at the next time step to allow for implicit timestepping schemes only.
+            d_in = {}
+            d_in['dt'] = np.real(t2_range[iteration+1])-np.real(t2_range[iteration])
+
+            d_in['A0'] = np.abs(bc_0[iteration+1, :])
+            d_in['A1'] = np.abs(bc_1[iteration+1, :])
+            d_in['phase0'] = np.angle(bc_0[iteration+1, :], deg=True)
+            d_in['phase1'] = np.angle(bc_1[iteration+1, :], deg=True)
+
+            d_in['Q1'] = Qarray[iteration+1]
+            if iteration==0:
+                IC = self.input.v('initial_condition_sediment')
+                d_in['initial_condition_sediment'] = IC
+                if IC == 'stock':
+                    d_in['Sinit'] = self.input.v('Sinit', range(0, jmax+1))
+                elif IC == 'erodibility':
+                    d_in['finit'] = self.input.v('finit', range(0, jmax+1))
+            else:
+                dc = self.block.getInput()
+                dc.merge(self.block.getOutput())
+                d_in['Sinit'] = dc.v('a', range(0, jmax+1), 0, 0)
+                d_in['initial_condition_sediment'] = 'stock'
+
+            ## 2. Run the model
+            self.block.addInputData(d_in)  # add the input to the controlled block of modules
+            init = False
+            if iteration==0:
+                init = True
+            self.block.run(init=init)
+
+            ## 3. Gather input and output from the block
+            dc = self.block.getInput()
+            dc.merge(self.block.getOutput())
+
+            ## 4. Process output
+            list_all_keys = dc.getAllKeys() # Create current subkeys-list
+            for qq, component in enumerate(self.OutputList):
+                self.subkeyList = [i[1:] for i in list_all_keys if component == i[0]]
+                for varkey in self.subkeyList:
+                    if component == varkey:     # if no subkeys were found, set the varkey to an empty tuple
+                        varkey = ()
+                    totalkey = (component,) + varkey
+
+                    # load the value as numerical data from the output
+                    f = np.arange(0, fmax+1)
+                    z = np.linspace(0,1, kmax+1)
+                    if self.verticalsize[qq]==1:
+                        z = [0]
+                    if self.frequencysize[qq]==1:
+                        f = [0]
+                    value = dc.v(component, *varkey, x=np.linspace(0,1, jmax+1), z=z, f=f)
+
+                    # After the first iteration, create the numpy array for computed values. Copy this data to the first and second time steps. The 0-index should be ignored
+                    if iteration == 0:
+                        store[totalkey] = np.expand_dims(value, axis=-1)
+
+                    store[totalkey] = np.append(store[totalkey], np.expand_dims(value, axis=-1), axis=-1)
+
+        ################################################################################################################
+        # Put output in MTS variables
+        ################################################################################################################
+        d = {}
+        for key in store.keys():
+            d = mergeDicts(d, self.setDictHierarchy(key, store))   # Merge existing dict with calculated data
+
+        return d
+        
+    def calc_subtidal_bcs(self):
+        """Define the seaward boundary conditions for all t.""" 
+        #np.tile is used to match the dimensions of t2 (nsteps,) and inputs (3,)
+        
+        ## Load input
+        AM0 = self.input.v('AM0')
+        AS0 = self.input.v('AS0')
+        AM1 = self.input.v('AM1')
+        AS1 = self.input.v('AS1')
+        
+        phaseM0 = self.input.v('phaseM0')
+        phaseS0 = self.input.v('phaseS0')
+        phaseM1 = self.input.v('phaseM1')
+        phaseS1 = self.input.v('phaseS1')
+        
+        sigmaM0 = self.input.v('OMEGA')
+        sigmaS0 = self.input.v('sigmaS2')
+
+        sigma_sn0 = (sigmaS0 - sigmaM0)
+        sigma_sn1 = 2*sigma_sn0
+        
+        num_cycles = self.input.v('num_sn_cycles')
+        period_sn0 = np.abs(2 * np.pi / (sigmaS0 - sigmaM0))
+        timeshift = self.input.v('time_shift')
+        if timeshift is not None:
+            t_axis = (self.input.v('MTS_grid', 'axis', 't').flatten()+timeshift)
+        else:
+            t_axis = self.input.v('MTS_grid', 'axis', 't').flatten()
+
+        t2_range = t_axis*period_sn0*num_cycles
+        
+        ## Process
+        AM0 = np.asarray(AM0, dtype='complex')
+        AM1 = np.asarray(AM1, dtype='complex')
+        
+        AS0 = np.tile(np.asarray(AS0, dtype='complex'), (len(t2_range), 1))  # Note that the amplitudes are tiled
+        AS1 = np.tile(np.asarray(AS1, dtype='complex'), (len(t2_range), 1))  # Note that the amplitudes are tiled
+        # Get phases IN RADIANS (used in this module!)
+        phaseM0 = np.asarray(phaseM0, dtype='complex')/180*np.pi
+        phaseS0 = np.asarray(phaseS0, dtype='complex')/180*np.pi
+        phaseM1 = np.asarray(phaseM1, dtype='complex')/180*np.pi
+        phaseS1 = np.asarray(phaseS1, dtype='complex')/180*np.pi
+
+        sigma_sn0 = np.asarray(sigma_sn0, dtype='complex')
+        sigma_sn1 = np.asarray(sigma_sn1, dtype='complex')
+        
+        T2_array = np.transpose(np.tile(t2_range, (len(phaseS0), 1))) # Make an array such that "t_2 + phase" can be calculated elementwise
+        
+        res_0 = np.tile(AM0 * np.exp(1j * phaseM0), (len(t2_range), 1)) #Dummy variable
+        res_1 = np.tile(AM1 * np.exp(1j * phaseM1), (len(t2_range), 1)) #Dummy variable
+        
+        # Define the leading and first order seaward boundary conditions
+        bc_0 = res_0 + AS0 * np.exp(1j * (sigma_sn0 * T2_array + np.tile(phaseS0, (len(t2_range), 1) )))
+        bc_1 = res_1 + AS1 * np.exp(1j * (sigma_sn1 * T2_array + np.tile(phaseS1, (len(t2_range), 1) )))
+
+        return bc_0, bc_1
+    
+    def setDictHierarchy(self, totalkey, store):
+        """convert the data from 'self.store' to a nestled dictionaries with a hierarchy according to the specified tuple 'totalkey' """
+        if len(totalkey)>1:
+            d = {totalkey[-1] : store[totalkey]} #store savedata in the lowest dictionary
+            for ii in reversed(totalkey[1:-1]):
+                d = {ii : d} #This is were we nestle the dict in the higher dict
+            d = {'MTS_'+totalkey[0]: d} #Give first key a 'MTS_' prefix to distinct it as a t2-dependent variable
+
+        if len(totalkey) == 1:
+            d = {'MTS_'+totalkey[0] : store[totalkey]}
+        d['__variableOnGrid'] = {}
+        d['__variableOnGrid']['MTS_'+totalkey[0]] = 'MTS_grid'
+        return d
+            
+        
